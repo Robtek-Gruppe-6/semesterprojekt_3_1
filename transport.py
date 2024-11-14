385f68e17ea41beb032bb142641e0b4b7ed27da2
Noffy28 <162138263+Noffy28@users.noreply.github.com>
2024-11-14 11:17:18 +0100

    First itteration of transport layer

--- transport.py
+++ transport.py
@@ -1,2 +1,20 @@
 
 
+
+class Transport:
+    def __init__(self, crc = True, segment = []):
+        self.crc = crc
+        self.segment = segment
+        pass
+    
+    def reciver_flowcontrol(self, crc, segment):
+        #Reciver side
+        data = segment
+        if(crc == True):
+            return True, data
+        
+        elif(crc == False):
+            return False, data
+        
+    def transmitter_flowcontrol(self,)
+        
