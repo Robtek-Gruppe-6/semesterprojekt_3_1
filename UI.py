35a8c418720657ebb871f6647c83cc02d88e0096
spkki <Svend.b.t@hotmail.dk>
2024-11-22 11:10:56 +0100

    Completed transmitter side maybe final 1.0

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 class UI():
     def __init__(self):
+        self.datalist = []
         pass
     
     def ui(self):
@@ -17,13 +18,17 @@ class UI():
                 print("Please enter distance in centimeters: ")
                 distance = input()
                 print ("Stacking command to drive forward " + distance + " centimeters")
-                return "A" + distance #Returns the command in the format "A" + distance
+                self.datalist.append("A")
+                self.datalist.extend(list(distance))
+                return self.datalist #Returns the command in the format "A" + distance
             
             elif direction == "2":
                 print( "Please enter distance in centimeters: ")
                 distance = input()
                 print("Stacking command to drive backward " + distance + " centimeters")
-                return "B" + distance #Returns the command in the format "B" + distance
+                self.datalist.append("B")
+                self.datalist.extend(list(distance))
+                return self.datalist #Returns the command in the format "B" + distance
             
             else:
                 print("Invalid input")
@@ -37,19 +42,26 @@ class UI():
                 print("Please enter degrees to turn: ")
                 degrees = input()
                 print("Stacking command to turn right " + degrees + " degrees")
-                return "C" + degrees #Returns the command in the format "C" + degrees
+                self.datalist.append("C")
+                self.datalist.extend(list(degrees))
+                return self.datalist #Returns the command in the format "C" + degrees
             
             elif direction == "2":
                 print("Please enter degrees to turn: ")
                 degrees = input()
                 print("Stacking command to turn left " + degrees + " degrees")
-                return "D" + degrees #Returns the command in the format "D" + degrees
+                self.datalist.append("D")
+                self.datalist.extend(list(degrees))
+                return self.datalist #Returns the command in the format "D" + degrees
             
             else:
                 print("Invalid input")
 
         if mode == "3":
             print("Sending all commands")
-            return "E"  #Returns the command "E" to send all commands
+            self.append("E")
+            return self.datalist  #Returns the command "E" to send all commands
+        
+ui = UI() 
             
-UI().ui()
+#UI().ui()
--- main.py
+++ main.py
@@ -19,47 +19,52 @@ def main():
     crc1_bool = False
     crc2_bool = False
     counting_done = False
-    data = ""
+    data = []
     length_val = 255
     
-    #framer.input_binary()
-
-    try:
-        for audio_chunk in micro.capture_audio():
-            filtered_chunk = fil.butter_bandpass(audio_chunk)
-            frequencies, magnitude = fil.analyze_frequency(filtered_chunk)
-
-            
-            binary_val = decoder.process_chunk(frequencies, magnitude)
-            #binary_val = int(input("input: "))
-            flowcontrol.reciver_flowcontrol(binary_val)
-            
-                    
-
-            
-            
-            
-                
-           
-           #if binary_val is not None:
-           #    result = datalinker.receive_data(binary_val)
-           #    if result:
-           #         
-           #         collected_data, data_length = result
-           #         print("Collected Data:", [bin(int(b, 2))[2:].zfill(4) for b in collected_data])
-           #         print("Data Length:", data_length)
-           
-           
-           
-    finally:
-        micro.close()
-        
+    #Transmitter side
+    data = ui.ui()
+    frame = framer.build_frame(data)
+    print(f"Frame: {frame}")
+    spk.play_list_of_tones(frame)
+    
+    
+#    framer.input_binary()
+#
+#    try:
+#        for audio_chunk in micro.capture_audio():
+#            filtered_chunk = fil.butter_bandpass(audio_chunk)
+#            frequencies, magnitude = fil.analyze_frequency(filtered_chunk)
+#
+#            
+#            binary_val = decoder.process_chunk(frequencies, magnitude)
+#            #binary_val = int(input("input: "))
+#            flowcontrol.reciver_flowcontrol(binary_val)
+#            
+#                    
+#
+#            
+#            
+#            
+#                
+#           
+#           #if binary_val is not None:
+#           #    result = datalinker.receive_data(binary_val)
+#           #    if result:
+#           #         
+#           #         collected_data, data_length = result
+#           #         print("Collected Data:", [bin(int(b, 2))[2:].zfill(4) for b in collected_data])
+#           #         print("Data Length:", data_length)
+#           
+#           
+#           
+#    finally:
+#        micro.close()
+#        
         
 
 
 if __name__ == "__main__":
-    #ui.run_example() #UI example code
-    #ui.run_protocol() #Runs the movementProtocol but it needs to display that into the UI SO NOT DONE!
     main()
     
 
