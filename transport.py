af39e6dba1d9b57de124b7db5f0f8b176bf6fcaf
PapaEchoRomeo <lukasvadmand@gmail.com>
2024-11-27 14:53:41 +0100

    "transmitter_add_lebel" added in transport.py

--- transport.py
+++ transport.py
@@ -8,6 +8,7 @@ class Transport:
         self.segment = segment
         self.crc_value = crc_value
         self.prev_parity = []
+        self.prev_lebel = 0
         pass
     
     def reciver_flowcontrol(self, no_error_detected, parity = "A", length = ['0', '0'], datadata = ['0']):
@@ -20,7 +21,18 @@ class Transport:
         elif(no_error_detected and parity == self.prev_parity[-1]):
             # Send ACK
             pass
-            
+
+    def transmitter_add_label(self, data):
+        new_data = []
+        if(self.prev_lebel == 0):
+            new_data.append(1)
+            new_data.append(data)
+            self.prev_lebel = 1
+        elif(self.prev_lebel == 1):
+            new_data.append(0)
+            new_data.append(data)
+            self.prev_lebel = 0
+        return new_data
             
         
 flowcontrol = Transport()
