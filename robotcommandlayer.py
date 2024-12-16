9b5e6450b0a57f1e5cbe8e75c08d0bf7e7a25ed0
PapaEchoRomeo <lukasvadmand@gmail.com>
2024-12-16 14:29:12 +0100

    outcommented stuff in "robotcommandlayer.py"

--- robotcommandlayer.py
+++ robotcommandlayer.py
@@ -9,34 +9,33 @@ class PresentationLayer:
             return None
     
     def parse_data(self, data):
-        n_mode = 1
-
-        mode = data[0]  # First `n_mode` nibbles
+        mode = data[0]  # First nibble is the mode
         
         if mode == 'E':
             return mode, 0
 
-        extended_mapping = {
-            "A": 10,
-            "B": 11,
-            "C": 12,
-            "D": 13,
-            "*": 14,
-            "#": 15
-        }
-
-        distance_hex = data[n_mode:]  # Remaining characters as distance
-
-        # Convert distance_hex character by character using the extended_mapping
-        distance_mapped = ""
-        for char in distance_hex:
-            if char in extended_mapping:
-                 distance_mapped += str(extended_mapping[char])  # Replace with mapped value
-            else:
-                distance_mapped += char  # Keep numeric values as is
+    #    extended_mapping = {
+    #        "A": 10,
+    #        "B": 11,
+    #        "C": 12,
+    #        "D": 13,
+    #        "E": 14,
+    #        "F": 15
+    #    }
+#
+    #    distance_hex = data[n_mode:]  # Remaining characters as distance
+#
+    #    # Convert distance_hex character by character using the extended_mapping
+    #    distance_mapped = ""
+    #    for char in distance_hex:
+    #        if char in extended_mapping:
+    #             distance_mapped += str(extended_mapping[char])  # Replace with mapped value
+    #        else:
+    #            distance_mapped += char  # Keep numeric values as is
 
         # Convert the mapped distance string to an integer
-        distance_int = int(distance_mapped)
+    #    distance_int = int(distance_mapped)
+        distance_int = int(data[1:])
 
         return mode, distance_int
 
